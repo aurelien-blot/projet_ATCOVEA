d3daafeb454a3f2eb160ea46b6f2267bd18e699e
SamLacroix <33868670+SamLacroix@users.noreply.github.com>
2017-11-29 17:14:54 +0100

    Update swat_db_valeurs_test.sql

--- swat_db_valeurs_test.sql
+++ swat_db_valeurs_test.sql
@@ -21,12 +21,12 @@ INSERT INTO `developpeur` (`nom_developpeur`, `prenom_developpeur`, `competences
 ('LEROY', 'Stéphane', 'HTML\r\nJS\r\nCSS', 'http://www.google.fr'),('GROS', 'Jean', 'JS', 'http://www.elec-auto.com'),
 ('BROUTE', 'Jean', 'JS', 'http://www.elec-auto.com'),('SATONS', 'louise', 'Java', 'http://www.elec-auto.com'),('LONS', 'MARIE', 'Java', 'http://www.elec-auto.com');
 
-INSERT INTO `demande` (`code_affaire_demande`, `libelle_demande`, `date_debut_demande`, `date_fin_demande`, `contexte_demande`, `details_prestation_demande`, `zone_geographique`, `id_interloc_technique`, `id_statut`,`id_interet`, `id_acheteur`) VALUES
-('123ABC', 'Modification de la fonction 1', '2017-11-22', '2017-11-30', 'contexte 1', 'détails demande 1', 'Le Mans', 2, 1, 1, 1),
-('GEL426', 'demande 2', '2017-11-23', '2017-11-28', 'contexte demande 2', 'détails 2', 'Nantes', 2, 2, 2, 3),
-('4FFZLD456', 'Libellé demande 3', '2017-11-25', '2017-11-28', 'contexte demande', 'détails prestation demande','Rennes', 1, 1, 2, 1),
-('5T886', 'Libellé demande 4', '2017-11-28', '2017-12-03', 'contexte demande', 'détails prestation demande','Orleans', 2, 2, 3, 2),
-('5ZLD886', 'Libellé demande 5', '2017-11-18', '2017-12-13', 'contexte demande5', 'détails prestation demande','Orleans', 3, 3, 2, 4);
+INSERT INTO `demande` (`code_affaire_demande`,`intitule_demande`, `libelle_demande`, `date_debut_demande`, `date_fin_demande`, `contexte_demande`, `details_prestation_demande`, `zone_geographique`, `id_interloc_technique`, `id_statut`,`id_interet`, `id_acheteur`) VALUES
+('123ABC', 'Modification de la fonction 1',`AT-5164-458A-Test`, '2017-11-22', '2017-11-30', 'contexte 1', 'détails demande 1', 'Le Mans', 2, 1, 1, 1),
+('GEL426', 'demande 2',`AB-5964-168B-Test`, '2017-11-23', '2017-11-28', 'contexte demande 2', 'détails 2', 'Nantes', 2, 2, 2, 3),
+('4FFZLD456', 'Libellé demande 3',`IT-165983-45F-Test`, '2017-11-25', '2017-11-28', 'contexte demande', 'détails prestation demande','Rennes', 1, 1, 2, 1),
+('5T886', 'Libellé demande 4',`KL-831-45869J-Test`, '2017-11-28', '2017-12-03', 'contexte demande', 'détails prestation demande','Orleans', 2, 2, 3, 2),
+('5ZLD886', 'Libellé demande 5',`AA-23659-45698PM-Test`, '2017-11-18', '2017-12-13', 'contexte demande5', 'détails prestation demande','Orleans', 3, 3, 2, 4);
 
 INSERT INTO `reponse` (`sous_traitant_reponse`, `nb_jours`, `taux_journalier`, `date_debut`, `date_fin`, `id_demande`) VALUES
 (0, 12, 200, '2017-11-26', '2017-11-30', 1),(1, 8, 150, '2017-11-22', '2017-11-30', 2),

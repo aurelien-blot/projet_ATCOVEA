c032c323882df6e0fb6e3b989a52b04d92712b53
julien-leduc <33868750+julien-leduc@users.noreply.github.com>
2018-01-17 15:33:04 +0100

    Update ajout_reponse.js
    simplification du code

--- site/js/ajout_reponse.js
+++ site/js/ajout_reponse.js
@@ -12,31 +12,15 @@ $.ajax(
 			console.log(mydata);
 	        $.each(mydata, function(i, obj) {
 	          $(".code_affaire").append("<option>" + obj.codeAffaireDemande + "</option>" );
+	          $(".profil_dev").append("<option value ='"+obj.idDeveloppeur+"'>" + obj.nomDeveloppeur + "  " + obj.prenomDeveloppeur + "</option>" );
+
 	        });
 	        }
 }
 );
 
-$.ajax(
-{
-    method: "GET",
-	url: "http://192.168.43.175:8080/ATCOVEA_RESTful/webresources/epsi.lemans.developpeur",
-	data: { },
-	headers : {
-		accept: "application/json"
-	},
-	success : function( mydata ) {
-			console.log(mydata);
-	        $.each(mydata, function(i, obj) {
-	          $(".profil_dev").append("<option value ='"+obj.idDeveloppeur+"'>" + obj.nomDeveloppeur + "  " + obj.prenomDeveloppeur + "</option>" );
-	        });
-	        }
-}
-);
 
 $(".profil_dev").change(function(){
-
-	//console.log($(".profil_dev").val());
 
 $.ajax(
 {
